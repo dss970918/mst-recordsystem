3cb7eccae64778bb61d971e1dc1c8ea6edf4fae0
yaozhao <xiaodai@qq.com>
2020-06-05 16:28:12 +0800

    feat: 🎸 发布设置

--- packages/picasso-testsuite/.qconfig.ts
+++ packages/picasso-testsuite/.qconfig.ts
@@ -1,6 +1,9 @@
 import { getProjectPath, IQConfig } from '@jtl/q-scripts';
 
 const qu: IQConfig = {
-    mode: 'lib'
+    mode: 'lib',
+    scripts:{
+        build:'skip build'
+    }
 };
 export default qu;
