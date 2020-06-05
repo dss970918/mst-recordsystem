0e2606bcbb05d363db6d4f1a545e75af574ba7d2
yaozhao <xiaodai@qq.com>
2020-06-05 16:24:42 +0800

    feat: 🎸 发布设置

--- .qconfig.ts
+++ .qconfig.ts
@@ -1,6 +1,9 @@
 import { getProjectPath, IQConfig } from '@jtl/q-scripts';
 
 const qu: IQConfig = {
-    mode: 'lib'
+    mode: 'lib',
+    publishConfig:{
+        gitUrl: 'git@116.62.100.239:jtl3d-faces/picasso-recordsystem.git'
+    }
 };
 export default qu;
